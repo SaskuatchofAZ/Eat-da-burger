720df92a33b19fc6724ff6358972df20de34e5ec
Saskuatch <asmithamongus@gmail.com>
2020-05-10 14:39:58 -0700

    first commit with data, app almost working

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('California Burger');
+INSERT INTO burgers (name) VALUES ('Western Bacon Chee');
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
+INSERT INTO burgers (name) VALUES ('Marilyn Manson Burger');
+

9a2463ee6faad5b0e3f5f01d4cbf700a096dd3f9
Saskuatch <asmithamongus@gmail.com>
2020-05-12 21:47:30 -0700

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "g8mh6ge01lu2z3n1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: null,
-  password: null,
-  database: null
-});
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: null,
+    password: null,
+    database: null
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
